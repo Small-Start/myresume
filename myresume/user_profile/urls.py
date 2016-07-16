c9525792327303e2bd3a1ff6bb2584daf1cf2669
jindalg <jindalshivam65@gmail.com>
2016-07-16 23:12:47 +0530

    URL.py and readme.md update

--- myresume/user_profile/urls.py
+++ myresume/user_profile/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
 	url(r'^$',views.index.as_view()),
 	url(r'^student/$', views.StudentView.as_view()),
-	url(r'^studentAPI/$', views.StudentAPI.as_view())
+	url(r'^studentAPI/$', views.StudentAPI.as_view()),
+	url(r'^student/(?P<pk>[\w.@+-]{1,30})/$', views.StudentDetail.as_view()),
+	# url(r'^education/$', views.EducationDetail.as_view())
 ]
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 New version
+
+API Documentation
+
+https://docs.google.com/document/d/1JGKP--ybNL6q0yLEqrxEkdSND5Jxl-HwVnZsXQ6rSac/edit?usp=sharing
